8c878383e1b7ac8a00dde48a8f9a62ccf4ca97b0
bmonagan <bernardmonagan@gmail.com>
2025-09-01 02:53:14 -0400

    Starting to clean up based on pylint conventions. Also adding in method and docstrings where missing. Along with any other needed improvements.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+"""
+Entry point for running YOLO training via the yolo_training module.
+"""
 import yolo_training
 
 def main():
@@ -5,9 +8,6 @@ def main():
     Main function to run YOLO training.
     """
     yolo_training.main()
-    
 
 if __name__ == "__main__":
     main()
-
-
