69b51bd7d50423d75d3acac4a2397f7ce9f50314
bmonagan <bernardmonagan@gmail.com>
2025-09-02 01:44:17 -0400

    Improved dependencies and config.

---
+++ config.py
@@ -0,0 +1,10 @@
+DETECTION_CSV_PATH = "path/to/detection.csv"
+DETECTION_OUTPUT = "path/to/detection_output.png"
+
+CONFIDENCE_CSV_PATH = "path/to/confidence.csv"
+CONFIDENCE_OUTPUT = "path/to/confidence_output.png"
+
+CSV_PATH = "path/to/csv_file.csv"
+GRAPHS_DIR = "path/to/graphs"
+
+MODEL_PATH = "path/to/model.pt"
